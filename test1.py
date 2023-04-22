566ec983f4cb20d665b9085763154f4b9c5eda87
dnp987 <dnp987@gmail.com>
2023-04-22 14:35:17 -0400

    test1 updated

--- test1.py
+++ test1.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     from datetime import datetime
     import re
     from tests.test2 import function1
+    from test3 import function2
     
     date_time = datetime.now().strftime('%Y %B %d %I %M %p') # get the date and time
     print (date_time)
@@ -10,6 +11,12 @@ if __name__ == "__main__":
     text = "This is a test"
     result = function1(number, text)
     if not result:
+        print ("test failed")
+    else:
+        print ("test passed")
+        
+    result2 = function2(7, "ABC")
+    if result:
         print ("test failed")
     else:
         print ("test passed")
